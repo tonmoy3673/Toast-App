c7ebb80cf245b658a4d88bbb3fcd69f7416e225a
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-03-13 10:54:19 +0600

    :lipstick: design the UI

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 console.log('connected');
-
+// ========== get toast container ========//
 const toastBox= document.getElementById('toast-container');
+
+// ========= set toast msg =======//
+let successMsg = 'Success! This Success Text!';
+let errorMsg = 'Error! This is Error Text!'
+let warnMsg ='Warning! This is warning Text!'
+
 
 
 // ============ display toast ==========//
+const displayToast=(msg)=>{
+    const toastDiv = document.createElement('div');
+    toastDiv.classList.add('toast-div');
+    const toast = document.createElement('p');
+    toast.innerHTML= msg;
+    toastDiv.appendChild(toast);
+    toastBox.appendChild(toastDiv)
+
+}
